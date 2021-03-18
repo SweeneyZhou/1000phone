aff55dbbf72492be95c27d091a05690705671a0e
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-18 19:08:41 +0800

    计算：-1/2+3/4-5/6+7/8+...+99/100

--- src/com/qianfeng/day0718/homework/Test7.java
+++ src/com/qianfeng/day0718/homework/Test7.java
@@ -1,22 +1,23 @@
 package com.qianfeng.day0718.homework;
+
 /*
-* 1.计算：-1/2+3/4-5/6+7/8+...+99/100
-* */
+ * 1.计算：-1/2+3/4-5/6+7/8+...+99/100
+ * */
 public class Test7 {
     public static void main(String[] args) {
-        double i=1;
-        double sum=0;
-        boolean flag=false;
-        while (i<=100){
-            if(i%2==0){
-                if(flag){
-                    sum+=(i-1)/i;
+        double i = 1;
+        double sum = 0;
+        boolean flag = false;
+        while (i <= 100) {
+            if (i % 2 == 0) {
+                if (flag) {
+                    sum += (i - 1) / i;
                     //System.out.printf("+"+(i-1)+"/"+i);
-                }else{
-                    sum-=(i-1)/i;
+                } else {
+                    sum -= (i - 1) / i;
                     //System.out.printf("-"+(i-1)+"/"+i);
                 }
-                flag=!flag;
+                flag = !flag;
             }
             i++;
         }
