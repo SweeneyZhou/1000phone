44fa00814f8821ef0f3ba9d5bc5c092d47d9b0f5
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-19 11:27:37 +0800

    判断一个字符串是否是回文的

--- src/com/qianfeng/day0719/Test1.java
+++ src/com/qianfeng/day0719/Test1.java
@@ -1,7 +1,20 @@
 package com.qianfeng.day0719;
 
+import java.util.Scanner;
+
+/*
+* 判断一个字符串是否是回文的
+* */
 public class Test1 {
     public static void main(String[] args) {
-
+        Scanner scanner =new Scanner(System.in);
+        String s=scanner.next();
+        for (int i=0;i<s.length();i++){
+            if (s.charAt(i)!=s.charAt(s.length()-i-1)){
+                System.out.println("不是回文");
+                return;
+            }
+        }
+        System.out.println("是回文");
     }
 }
