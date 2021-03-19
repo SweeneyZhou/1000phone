b9d19952955d4bfd2d07c9ae8b5e87e5b7ee3228
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-19 21:02:21 +0800

    将用户输入的任意内容拼接成一段完整的内容，当用户输入“xxx”时结束，并输出这段话

--- src/com/qianfeng/day0319/homework/Test3.java
+++ src/com/qianfeng/day0319/homework/Test3.java
@@ -6,20 +6,17 @@ import java.util.Scanner;
  * 将用户输入的任意内容拼接成一段完整的内容，当用户输入“xxx”时结束，并输出这段话
  * */
 public class Test3 {
+    static final String STOP = "xxx";
     public static void main(String[] args) {
-        fun1();
-        //fun2();
-    }
-
-    static void fun1() {
         Scanner scanner = new Scanner(System.in);
         StringBuilder stringBuilder = new StringBuilder();
         for (String s; ; ) {
             s = scanner.next();
-            if (!s.contains("xxx")) {
+            if (!s.contains(STOP)) {
                 stringBuilder.append(s);
             } else {
-                s = s.substring(0, s.indexOf("xxx"));
+                //s = s.substring(0, s.indexOf(STOP));//不输出xxx
+                s = s.substring(0, s.indexOf(STOP)+3);//输出xxx
                 stringBuilder.append(s);
                 System.out.println("用户输入了:" + stringBuilder.toString());
                 return;
--- src/com/qianfeng/day0319/homework/Test3_2.java
+++ src/com/qianfeng/day0319/homework/Test3_2.java
@@ -12,8 +12,9 @@ public class Test3_2 {
         do {
             s1 = scanner.next();
             s = s + s1;
-        } while (!s1.contains("xxx"));
-
+        } while (!s1.contains(STOP));
+        s = s.substring(0, s.indexOf(STOP) + 3);//输出xxx
+        //s = s.substring(0, s.indexOf(STOP);//不输出xxx
         System.out.println("用户输入了:" + s);
     }
 }
