e6110a42dc86f368b63a8ac1b9f57a74e8f101a1
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-22 17:19:26 +0800

    initial

---
+++ src/com/qianfeng/day0322/Test.java
@@ -0,0 +1,12 @@
+package com.qianfeng.day0322;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        double[] a=new double[5];
+        a[0]=1.2;
+        System.out.println(Arrays.toString(a));
+
+    }
+}
