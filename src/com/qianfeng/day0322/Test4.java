7dffb1e2d1f4622d78928f1da2d0aabd227e8aa6
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-22 14:56:38 +0800

    3、用户通过键盘输入任意内容，拼接成一句语句，当输入！时，将这句话输出当用户输入x时，将用书输入的所有内容输出

---
+++ src/com/qianfeng/day0322/Test4.java
@@ -0,0 +1,31 @@
+package com.qianfeng.day0322;
+
+import java.util.Scanner;
+
+/*
+ * 3、
+ * 用户通过键盘输入任意内容，拼接成一句语句，当输入！时，将这句话输出
+ * 当用户输入x时，将用书输入的所有内容输出
+ * */
+public class Test4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder str = new StringBuilder();
+        String s1,s2="";
+
+        while(true) {
+            System.out.println("请输入：");
+            s1 = scanner.nextLine();
+            if ("x".equals(s1)) {
+                System.out.println(s2);
+            } else if ("!".equals(s1)) {
+                System.out.println(str);
+                s2="";
+                str = new StringBuilder();
+            } else {
+                s2=s1;
+                str.append(s2).append("\n");
+            }
+        }
+    }
+}
