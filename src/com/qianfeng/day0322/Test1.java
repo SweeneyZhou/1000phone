904fa84dea973eac0cd5f329e02d951ae622b641
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-22 11:39:59 +0800

    打印九九乘法表

--- src/com/qianfeng/day0322/Test1.java
+++ src/com/qianfeng/day0322/Test1.java
@@ -7,7 +7,7 @@ public class Test1 {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i + "*" + j + "=" + i * j);
+                System.out.print( j + "*" + i + "=" + i * j);
                 if (j!=i) System.out.print("\t");
             }
             if (i != 9) System.out.println();
