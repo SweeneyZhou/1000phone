f8d723d34b7b4c7382828401da1569f64925a5b8
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-22 18:08:17 +0800

    5、用数组打印斐波那契数列的前15项

---
+++ src/com/qianfeng/day0322/homework/Test5.java
@@ -0,0 +1,27 @@
+package com.qianfeng.day0322.homework;
+
+import java.util.Arrays;
+
+/*
+ * 5、用数组打印斐波那契数列的前15项
+ * */
+public class Test5 {
+    static final int N = 15;
+
+    public static void main(String[] args) {
+        int[] fab = new int[N];
+        if(N==0) {
+            System.out.println("[]");
+            return;
+        }else if(N==1){
+            System.out.println("[1]");
+            return;
+        }
+        fab[0] = 1;
+        fab[1] = 1;
+        for (int i = 2; i < N; i++) {
+            fab[i] = fab[i - 2] + fab[i - 1];
+        }
+        System.out.println(Arrays.toString(fab));
+    }
+}
