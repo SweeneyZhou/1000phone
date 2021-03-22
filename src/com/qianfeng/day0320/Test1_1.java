6a969fbf83211513605410bc9ac07d4a7ccfc7f7
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-22 09:32:25 +0800

    1、随机生成6组4位字符组成的验证码。验证码由大小写字母、数字字符组成。

--- src/com/qianfeng/day0320/Test1_1.java
+++ src/com/qianfeng/day0320/Test1_1.java
@@ -19,7 +19,7 @@ public class Test1_1 {
         for (int i=0;i<TOTAL;i++){
             StringBuilder valid=new StringBuilder();
             for(int j=0;j<LENGTH;j++){
-                valid.append(res.charAt(random.nextInt(res_len-1)));
+                valid.append(res.charAt(random.nextInt(res_len)));
             }
             System.out.println(valid);
         }
