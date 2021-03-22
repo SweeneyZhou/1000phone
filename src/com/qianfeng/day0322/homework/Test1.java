bf0d65ba8ad70db96598e8b3b017309c55c7cee9
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-22 17:38:09 +0800

    1、输出 int数组中能被3整除的元素

--- src/com/qianfeng/day0322/homework/Test1.java
+++ src/com/qianfeng/day0322/homework/Test1.java
@@ -3,14 +3,14 @@ package com.qianfeng.day0322.homework;
 import java.util.Arrays;
 
 /*
-* 1、输出 int数组中能被3整除的元素
-* */
+ * 1、输出 int数组中能被3整除的元素
+ * */
 public class Test1 {
     public static void main(String[] args) {
-        int[] arr1={1,2,123,123123,23321,6644};
-        System.out.println("在"+ Arrays.toString(arr1)+"中能被3整除的元素:");
+        int[] arr1 = {1, 2, 123, 123123, 23321, 6644};
+        System.out.println("在" + Arrays.toString(arr1) + "中能被3整除的元素:");
         for (int i : arr1) {
-            if (i%3==0) System.out.println(i);
+            if (i % 3 == 0) System.out.println(i);
         }
     }
 }
