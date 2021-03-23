e60c761c214bc815ba2e42fe9426dd15f3bc8a45
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-23 20:41:55 +0800

    4.将一个nextInt(9)+1的随机数赋值给一个长度为9的int数组，保证不能重复

--- src/com/qianfeng/day0323/homework/Test4.java
+++ src/com/qianfeng/day0323/homework/Test4.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 import java.util.Random;
 
 /*
-* 4.将一个nextInt(9)+1的随机数赋值给一个长度为9的int数组，保证不能重复
-* */
+ * 4.将一个nextInt(9)+1的随机数赋值给一个长度为9的int数组，保证不能重复
+ * */
 public class Test4 {
     public static void main(String[] args) {
-        int[] a=new int[9];
-        Random random=new Random();
+        int[] a = new int[9];
+        Random random = new Random();
         for (int i = 0; i < a.length; i++) {
-            int t=random.nextInt(9)+1;
-            a[i]=t;
+            int t = random.nextInt(9) + 1;
+            a[i] = t;
             for (int j = 0; j < i; j++) {
-                if(a[j]==t){
-                    a[i]=0;
+                if (a[j] == t) {
+                    a[i] = 0;
                     i--;
                 }
             }
