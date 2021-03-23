7d6b263c4f84440c85a99a77532e9969cd11c19c
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-23 09:55:25 +0800

    5、用数组打印斐波那契数列的前15项和第15项

---
+++ src/com/qianfeng/day0323/Test.java
@@ -0,0 +1,12 @@
+package com.qianfeng.day0323;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr={3,4,643,645,6543,34};
+        int sum=0;
+        for (int i : arr) {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
