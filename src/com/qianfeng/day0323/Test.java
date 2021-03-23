969ad11de043eb326d2208fa8c76e104a7d22475
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-23 09:56:18 +0800

    initial

--- src/com/qianfeng/day0323/Test.java
+++ src/com/qianfeng/day0323/Test.java
@@ -2,10 +2,10 @@ package com.qianfeng.day0323;
 
 public class Test {
     public static void main(String[] args) {
-        int[] arr={3,4,643,645,6543,34};
-        int sum=0;
+        int[] arr = {3, 4, 643, 645, 6543, 34};
+        int sum = 0;
         for (int i : arr) {
-            sum+=i;
+            sum += i;
         }
         System.out.println(sum);
     }
