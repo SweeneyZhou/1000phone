9b29200a98bbc497e618b1c48f7a1b7a272f6a68
sweeneyzhou <sweeneyzhou@outlook.com>
2021-03-23 20:43:04 +0800

    3.将一个int数组打乱{1,2,3,4,5,6,7,8,0}(2种方法) 方法一：遍历每一个位置，每次生成一个新的位置，交换这两个位置上的值

--- src/com/qianfeng/day0323/homework/Test3.java
+++ src/com/qianfeng/day0323/homework/Test3.java
@@ -9,7 +9,7 @@ import java.util.Random;
 * */
 public class Test3 {
     public static void main(String[] args) {
-        int[] a={1,2,3,4,5,6,7,8,9,0};
+        int[] a={1,2,3,4,5,6,7,8,9,0,32,76};
         Random r=new Random();
         //遍历每一个位置，每次生成一个新的位置，交换这两个位置上的值
         for (int i = 0; i < a.length; i++) {
