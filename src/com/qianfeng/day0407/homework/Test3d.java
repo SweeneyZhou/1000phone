6eeac5f038689efdbe1a4df16638d4e5a822a5a2
sweeneyzhou <sweeneyzhou@outlook.com>
2021-04-07 20:32:11 +0800

    d.找出字符串中所有连续的"abc"

--- src/com/qianfeng/day0407/homework/Test3d.java
+++ src/com/qianfeng/day0407/homework/Test3d.java
@@ -5,6 +5,8 @@ package com.qianfeng.day0407.homework;
 public class Test3d {
     public static void main(String[] args) {
         String abc="abcsafalslabcsdfaslabckjlasabcfdssab";
+        System.out.println("字符串:"+abc);
+        System.out.println("其中abc出现的位置:");
         for (int i=0,len=abc.length();i<len;i++){
             i=abc.indexOf("abc",i);
             if(i==-1)break;
